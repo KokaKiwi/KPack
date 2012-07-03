8635f338c52d19ce92379581e852949c02ba9a9c
KokaKiwi <admin@kokabsolu.com>
2012-07-03 15:46:22 +0200

    Updated Smarty

--- core/modules/kp_smarty/module.php
+++ core/modules/kp_smarty/module.php
@@ -1,12 +1,13 @@
 <?php
-require dirname(__FILE__) . "/smarty-3.0.6/Smarty.class.php";
+require dirname(__FILE__) . "/smarty/Smarty.class.php";
 
-KPack::register_module("kp_smarty", "Smarty Template Engine for KPack", "3.0.6");
+KPack::register_module("kp_smarty", "Smarty Template Engine for KPack", "1.0.0");
 KPack::register_class("kp_smarty", "KP_Smarty");
 
 class KP_Smarty extends Module
 {
-
+    var $smarty;
+    
     function __construct ()
     {
         $this->smarty = new Smarty();
